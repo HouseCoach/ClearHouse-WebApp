772402712d443452d858330fad3ed2de15996067
daeun <rlaekdms901@naver.com>
2025-08-31 21:29:45 +0900

    feat: 마이페이지 구현

--- src/components/layout/TopLayout.jsx
+++ src/components/layout/TopLayout.jsx
@@ -1,13 +1,17 @@
 import { useNavigate } from 'react-router-dom';
 
 import backIcon from '../../assets/back-icon.png';
-export default function TopLayout({ children }) {
+export default function TopLayout({ children, title, icon }) {
   const navigate = useNavigate();
   return (
     <div className="h-full pt-[56px]">
-      <div className="px-[18px] h-[28px] mb-[116px]">
+      <div className="px-[18px] h-[28px] mb-[116px] flex justify-between items-center">
         <button onClick={() => navigate(-1)}>
           <img src={backIcon} alt="backIcon" className="w-[28px] h-[28px]" />
+        </button>
+        <h1 className="text-[20px]">{title}</h1>
+        <button className="w-[29px] h-[20px]">
+          <img src={icon} alt="icon" className="w-[18px] h-[20px]" />
         </button>
       </div>
       <div className="w-full">{children}</div>
